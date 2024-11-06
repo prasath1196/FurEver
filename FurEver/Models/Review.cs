0a4fc68fb952fd6bb487bb861261453cf5f64ac7
Vivek Maddukuri <maddukvk@mail.uc.edu>
2024-11-06 12:29:21 -0500

    Adding Review Model to store the user reviews on the different dog breeds

---
+++ FurEver/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurEver.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        [StringLength(30)]
+        public string? DogBreedName { get; set; }
+        [StringLength(30)]
+        public string? UserName { get; set; }
+        public string? Comments { get; set; }
+        public int Rating { get; set; }
+    }
+}
