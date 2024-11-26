9ece0533b1803e13df6a31d9e7cf9251cef1a240
prasath1196 <prasath1196@gmail.com>
2024-11-25 20:07:39 -0500

    Remobed repetitive code from ReviewPage and integrated with API service class.

--- FurEver/Pages/ReviewPage.cshtml.cs
+++ FurEver/Pages/ReviewPage.cshtml.cs
@@ -1,3 +1,4 @@
+using FurEver.API_Data;
 using FurEver.Data;
 using FurEver.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -47,44 +48,17 @@ namespace FurEver.Pages
 
         private async Task LoadDogBreedsAsync()
         {
-            var apiUrl = "https://registry.dog/api/v1"; 
 
-            using (var httpClient = new HttpClient())
+            var apiResult = await DogBreedService.FetchBreedsAsync();
+
+            if (apiResult != null)
             {
-                var response = await httpClient.GetAsync(apiUrl);
-                if (response.IsSuccessStatusCode)
+                foreach (var breed in apiResult)
                 {
-                    var jsonResponse = await response.Content.ReadAsStringAsync();
-
-                    // Deserialize the JSON response
-                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                    var apiResult = JsonSerializer.Deserialize<ApiResult>(jsonResponse, options);
-
-                    if (apiResult != null && apiResult.Status == "success")
-                    {
-                        foreach (var breed in apiResult.Data)
-                        {
-                            DogBreeds.Add(breed.General.Name);
-                        }
-                    }
+                    DogBreeds.Add(breed.General.Name);
                 }
             }
-        }
-
-        public class ApiResult
-        {
-            public string? Status { get; set; }
-            public List<DogBreed> Data { get; set; }
-        }
-
-        public class DogBreed
-        {
-            public GeneralInfo General { get; set; }
-        }
-
-        public class GeneralInfo
-        {
-            public string Name { get; set; }
+            
         }
     }
 }
